8ae1df0d6a15b1d93f01a113ac40d0ef2681676b
Andrey <owner@Owners-MacBook-Air.local>
2022-10-28 13:20:35 +0300

    added lesson 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,48 @@
 $(function(){
  
+$('.review__slider').slick({
+    dots: true,
+    fade: true,
+});
+
+
+$(function() {
+    $(".slick-arrow").click(function() {
+        $(".slick-arrow").removeClass("btn-active");         
+        $(this).toggleClass("btn-active");
+    })
+});
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 10) {
+        $('.header').addClass('sticky'),
+        $('.menu').addClass('stick')
+    } else {
+        $('.header').removeClass('sticky'),
+        $('.menu').removeClass('stick')
+    }
+});
+
+$(".header,.footer").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+
+        if (id === '#popular') {
+            top = top - 200;
+        }
+
+        else if (id === '#footer') {
+            top = top - 700;
+        }
+
+        else if (id === '#about', '#skills', '#services', '#reviews', '#contact') {
+            top = top - 50;
+        }
+            
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+var mixer = mixitup('.products');
+
 });
