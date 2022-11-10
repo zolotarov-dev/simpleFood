b09ff62bea543a82596e2aa33181c17664e25919
Andrey <owner@Owners-MacBook-Air.local>
2022-11-10 15:45:48 +0200

    add responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,26 @@
-$(function(){
- 
-$('.review__slider').slick({
-    dots: true,
-    fade: true,
-});
+$(function () {
 
+    $('.review__slider').slick({
+        dots: true,
+        fade: true,
+        infinite: false,
+    });
+    
 
-$(function() {
-    $(".slick-arrow").click(function() {
-        $(".slick-arrow").removeClass("btn-active");         
-        $(this).toggleClass("btn-active");
-    })
-});
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() >= 10) {
+            $('.header').addClass('sticky'),
+                $('.menu').addClass('stick')
+        } else {
+            $('.header').removeClass('sticky'),
+                $('.menu').removeClass('stick')
+        }
+    });
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 10) {
-        $('.header').addClass('sticky'),
-        $('.menu').addClass('stick')
-    } else {
-        $('.header').removeClass('sticky'),
-        $('.menu').removeClass('stick')
-    }
-});
-
-$(".header,.footer").on("click","a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-      top = $(id).offset().top;
+    $(".header,.footer").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
 
         if (id === '#popular') {
             top = top - 200;
@@ -39,10 +33,59 @@ $(".header,.footer").on("click","a", function (event) {
         else if (id === '#about', '#skills', '#services', '#reviews', '#contact') {
             top = top - 50;
         }
-            
-    $('body,html').animate({scrollTop: top}, 1500);
-  });
 
-var mixer = mixitup('.products');
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+    var mixer = mixitup('.products');
 
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const burger = document.querySelector('.menu__burger');
+    const mobileMenu = document.querySelector('.sidebar-nav');
+    const bodyLock = document.querySelector('body');
+    const buttonClose = document.querySelector('.button-close');
+    const footerCon = document.querySelector('.footer__contacts');
+
+    burger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('sidebar-nav-active');
+        if (mobileMenu.classList.contains('sidebar-nav-active')) {
+            bodyLock.classList.add('lock');
+            buttonClose.classList.add('button-close-active');
+            footerCon.classList.add('footer__contacts-active');
+        }
+        else { 
+            bodyLock.classList.remove('lock');
+            buttonClose.classList.remove('button-close-active');
+            footerCon.classList.remove('footer__contacts-active');
+        }
+    });
+
+    buttonClose.addEventListener('click', () => {
+        mobileMenu.classList.toggle('sidebar-nav-active');
+        if (mobileMenu.classList.contains('sidebar-nav-active')) {
+            bodyLock.classList.add('lock');
+            buttonClose.classList.add('button-close-active');
+            footerCon.classList.add('footer__contacts-active');
+        }
+        else { 
+            bodyLock.classList.remove('lock');
+            buttonClose.classList.remove('button-close-active');
+            footerCon.classList.remove('footer__contacts-active');
+        }
+    });
+});
+
+
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+    $('.restaurants__items').slick({
+        dots: true,
+        fade: true,
+        arrows: false,
+    });
+  } 
