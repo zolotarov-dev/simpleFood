29ad65596899f2a08eb3809bcc5125e32214d212
zolotarov-dev <zola7177@gmail.com>
2023-08-16 21:42:17 +0300

    commit on new mac

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,86 @@
 $(function () {
 
+    $(".star").rateYo({
+        rating: 4,
+        starWidth: '15px',
+        spacing   : '6px',
+        starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.0229731 6.16432C0.0780973 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64481L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141566 6.64478C0.0138168 6.52028 -0.0322151 6.334 0.0229731 6.16432Z"/></svg>'
+        
+      });
+
+    var QtyInput = (function () {
+        var $qtyInputs = $(".qty-input");
+    
+        if (!$qtyInputs.length) {
+            return;
+        }
+    
+        var $inputs = $qtyInputs.find(".product-qty");
+        var $countBtn = $qtyInputs.find(".qty-count");
+        var qtyMin = parseInt($inputs.attr("min"));
+        var qtyMax = parseInt($inputs.attr("max"));
+    
+        $inputs.change(function () {
+            var $this = $(this);
+            var $minusBtn = $this.siblings(".qty-count--minus");
+            var $addBtn = $this.siblings(".qty-count--add");
+            var qty = parseInt($this.val());
+    
+            if (isNaN(qty) || qty <= qtyMin) {
+                $this.val(qtyMin);
+                $minusBtn.attr("disabled", true);
+            } else {
+                $minusBtn.attr("disabled", false);
+                
+                if(qty >= qtyMax){
+                    $this.val(qtyMax);
+                    $addBtn.attr('disabled', true);
+                } else {
+                    $this.val(qty);
+                    $addBtn.attr('disabled', false);
+                }
+            }
+        });
+    
+        $countBtn.click(function () {
+            var operator = this.dataset.action;
+            var $this = $(this);
+            var $input = $this.siblings(".product-qty");
+            var qty = parseInt($input.val());
+    
+            if (operator == "add") {
+                qty += 1;
+                if (qty >= qtyMin + 1) {
+                    $this.siblings(".qty-count--minus").attr("disabled", false);
+                }
+    
+                if (qty >= qtyMax) {
+                    $this.attr("disabled", true);
+                }
+            } else {
+                qty = qty <= qtyMin ? qtyMin : (qty -= 1);
+                
+                if (qty == qtyMin) {
+                    $this.attr("disabled", true);
+                }
+    
+                if (qty < qtyMax) {
+                    $this.siblings(".qty-count--add").attr("disabled", false);
+                }
+            }
+    
+            $input.val(qty);
+        });
+    })();
+
     $('.review__slider').slick({
         dots: true,
+        fade: true,
+        infinite: false,
+    });
+
+    $('.product-about__slider').slick({
+        dots: false,
         fade: true,
         infinite: false,
     });
@@ -214,3 +293,5 @@ inputs.forEach(function (input, handle) {
         }
     });
 });
+
+
